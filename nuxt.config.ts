ebf0c3d74b713c457513228e14d7751e9427b764
Sandip-44 <77221068+Sandip-44@users.noreply.github.com>
2025-05-18 22:58:19 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 export default defineNuxtConfig({
+    devServer: {
+    port: process.env.NUXT_PORT || 3000,
+    host: process.env.NUXT_HOST || '0.0.0.0'
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
